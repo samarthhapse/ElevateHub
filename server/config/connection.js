b61e64f77244570363051432f3926a5bbdd5977c
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2024-07-23 00:29:19 +0530

    new ui

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,25 @@
-import mongoose from "mongoose";
+// connection.js
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGO_URI}` || "");
-    console.log("database connection established");
-    
-  } catch (error) {
-    console.log("MONGODB connection FAILED ", error);
-    process.exit(1);
-  }
+    try {
+        const uri = process.env.MONGO_URI;
+        // console.log('MongoDB URI:', uri);
+        if (!uri) {
+            throw new Error('MONGO_URI is not defined in the environment variables');
+        }
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 };
+
 export default connectDB;
